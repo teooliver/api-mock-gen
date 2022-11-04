53f341331c6a00a07b08a7b4ef1f01e5f0386ebf
Teo Oliver <teo.liz.oli@gmail.com>
2022-11-04 23:24:20 +0000

    feat: write vec<tasks> to json pretty

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
-use chrono::Utc;
-use chrono::{prelude::*, Duration};
-use fake::faker::{self};
-use rand::Rng;
+use std::fs::File;
+use std::io::BufWriter;
 
+use chrono::DateTime;
+use chrono::Duration;
+use chrono::Utc;
+use fake::faker::lorem::en::*;
+use fake::Fake;
+use rand::Rng;
+use serde::{self, Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 struct User {
+    id: String,
     name: String,
     email: String,
 }
 
-struct Todo {
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Task {
     id: i32,
     name: String,
-    status: TodoItemStatus,
+    status: TaskStatus,
     user: User,
-    started_at: String,
-    finished_at: String,
+    started_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+    finished_at: DateTime<Utc>,
     color: String,
 }
 
-enum TodoItemStatus {
+#[derive(Clone, Debug, Deserialize, Serialize)]
+enum TaskStatus {
     Done,
     InProgress,
     NotNedeed,
     ReadyToStart,
 }
-
- 
 
 pub const PROJECT_COLORS: [&str; 10] = [
     "#61e294ff",
@@ -42,45 +51,78 @@ pub const PROJECT_COLORS: [&str; 10] = [
 
 pub const TIME_IN_SECONDS_OPTIONS: [i32; 7] = [3600, 1800, 5400, 3450, 1600, 1954, 7200];
 
-impl Todo{
-    fn newRandomTodo() -> Todo {
-
+impl Task {
+    fn new_random_todo() -> Task {
         let random_amount_of_days = rand::thread_rng().gen_range(0..=10);
-         let amount_of_days = Duration::days(random_amount_of_days);
-        let random_time_in_seconds = TIME_IN_SECONDS_OPTIONS[rand::thread_rng().gen_range(0..TIME_IN_SECONDS_OPTIONS.len())];
+        let amount_of_days = Duration::days(random_amount_of_days);
+        let random_time_in_seconds =
+            TIME_IN_SECONDS_OPTIONS[rand::thread_rng().gen_range(0..TIME_IN_SECONDS_OPTIONS.len())];
 
         let random_date = Utc::now() - amount_of_days;
 
-    let fake_initial_date = random_date - Duration::seconds(random_time_in_seconds as i64);
-    let fake_end_date = random_date + Duration::seconds(random_time_in_seconds as i64);
+        let fake_initial_date = random_date - Duration::seconds(random_time_in_seconds as i64);
+        let fake_end_date = random_date + Duration::seconds(random_time_in_seconds as i64);
 
+        Task {
+            id: 100,
+            name: Words(3..5).fake::<Vec<String>>().join(" "),
+            status: TaskStatus::Done,
+            user: User {
+                id: "100".to_string(),
+                name: fake::faker::name::en::FirstName()
+                    .fake::<String>()
+                    .to_string(),
+                email: fake::faker::name::en::FirstName()
+                    .fake::<String>()
+                    .to_string(),
+            },
+            started_at: fake_initial_date,
+            updated_at: fake_end_date,
+            finished_at: fake_end_date,
+            color: rand::thread_rng()
+                .gen_range(0..(PROJECT_COLORS.len() - 1))
+                .to_string(),
+        }
+    }
 
-        Todo {
-           id: 100,
-    name: fake::faker::lorem::en::Words(10).fake::<String>().to_string(),
-    status: TodoItemStatus::Done,
-    user: User {
-       name: fake:faker::name::en::FirstName().fake::<String>().to_string(),
-       email: fake:faker::name::en::FirstName().fake::<String>().to_string(),
-    },
-    started_at: "xxxxxx".to_string(),
-    finished_at: "xxxxxx".to_string(),
-    color: rand::thread_rng().gen_range(0..(PROJECT_COLORS.len() - 1)).to_string(),  
+    fn get_random_task_status() -> TaskStatus {
+        // TODO: use this when stable so we dont have to
+        // hard code the enum length in the `gen_range`, that way we can
+        // avoid breaking functionality with the enum changes.
+        //
+        // https://github.com/rust-lang/rust/issues/73662
+        // let enum_length = mem::variant_count::<TaskStatus>();
+
+        match rand::thread_rng().gen_range(0..=4) {
+            0 => TaskStatus::Done,
+            1 => TaskStatus::InProgress,
+            2 => TaskStatus::NotNedeed,
+            3 => TaskStatus::ReadyToStart,
+            _ => TaskStatus::ReadyToStart,
         }
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    // let input_path = std::env::args().nth(1).unwrap();
+    // let output_path = std::env::args().nth(2).unwrap();
+
+    let todos = generate_todo_list_struc(10);
+    let random_enum_value = Task::get_random_task_status();
+    // println!("{:?}", random_enum_value);
+    generateJsonDb(&todos, "tasks_json_db.json".to_string());
 }
 
-// fn generateJsonDb() {}
+fn generateJsonDb(tasks: &Vec<Task>, output_path: String) {
+    let mut writer = BufWriter::new(File::create("tasks_json_db.json").unwrap());
+    serde_json::to_writer_pretty(&mut writer, &tasks).unwrap();
+}
 
-fn generateTodoListStruc(amount: u8) -> Vec<Todo> {
-    let mut todos: Vec<Todo> = vec![];
+fn generate_todo_list_struc(amount: u8) -> Vec<Task> {
+    let mut todos: Vec<Task> = vec![];
 
-    for _n in 1..amount {
-        todos.push(Todo::newRandomTodo());
+    for _n in 1..=amount {
+        todos.push(Task::new_random_todo());
     }
 
     todos
